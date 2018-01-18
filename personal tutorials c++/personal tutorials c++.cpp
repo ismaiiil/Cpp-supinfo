90182d9d4e0c81718e30d110b668eeea1f59d5e4
mael <ismaiiil@live.com>
2018-01-18 20:25:35 +0400

    Add project files.

---
+++ personal tutorials c++/personal tutorials c++.cpp
@@ -0,0 +1,17 @@
+// personal tutorials c++.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	for (; ; ) {
+		printf("This loop will run forever.\n");
+	}
+
+	return 0;
+}
+
